c98a1cf8acae8554a836cbaba71786907eb5ae52
Mike <performantdata@users.noreply.github.com>
2022-04-01 15:49:59 -0700

    reinterpret the coloring algo
    I think I misunderstood the coloring sub-problem as intended.  This
    replaces the color mapping that standardizes the center cubie colors to
    one that standardizes the colors of the corner cubies and their order.
    
    This still leaves 24*24=576 "standardized" variations.

--- src/main/scala/cube.scala
+++ src/main/scala/cube.scala
@@ -1,43 +1,106 @@
+import Cube.colorMapByCorners
+
 case class Row(_1: Char, _2: Char, _3: Char) {
   override def toString: String = s"${_1} ${_2} ${_3}"
 
   def colorMap(m: Map[Char, Char]): Row = Row(m(_1), m(_2), m(_3))
 }
 
-case class Face(row1: Row, row2: Row, row3: Row) {
+case class Face(_1: Row, _2: Row, _3: Row) {
   override def toString: String =
-    s"""$row1
-       |$row2
-       |$row3
+    s"""${_1}
+       |${_2}
+       |${_3}
        |""".stripMargin
 
-  def colorMap(m: Map[Char, Char]): Face = Face(row1.colorMap(m), row2.colorMap(m), row3.colorMap(m))
+  def colorMap(m: Map[Char, Char]): Face = Face(_1.colorMap(m), _2.colorMap(m), _3.colorMap(m))
 }
 
 case class Cube(U: Face, L: Face, F: Face, R: Face, B: Face, D: Face) {
   override def toString: String =
-    s"""      ${U.row1}
-       |      ${U.row2}
-       |      ${U.row3}
-       |${L.row1} ${F.row1} ${R.row1} ${B.row1}
-       |${L.row2} ${F.row2} ${R.row2} ${B.row2}
-       |${L.row3} ${F.row3} ${R.row3} ${B.row3}
-       |      ${D.row1}
-       |      ${D.row2}
-       |      ${D.row3}
+    s"""      ${U._1}
+       |      ${U._2}
+       |      ${U._3}
+       |${L._1} ${F._1} ${R._1} ${B._1}
+       |${L._2} ${F._2} ${R._2} ${B._2}
+       |${L._3} ${F._3} ${R._3} ${B._3}
+       |      ${D._1}
+       |      ${D._2}
+       |      ${D._3}
        |""".stripMargin
 
   def colorMap(m: Map[Char, Char]): Cube =
     Cube(U.colorMap(m), L.colorMap(m), F.colorMap(m), R.colorMap(m), B.colorMap(m), D.colorMap(m))
 
-  def canonicalize: Cube = standardizeCenterColors
+  def cornerColors: CornersColoring = CornersColoring(Seq(
+    CornerColors(F._1._1, U._3._1, L._1._3), CornerColors(F._1._3, R._1._1, U._3._3),
+    CornerColors(F._3._3, D._1._3, R._3._1), CornerColors(F._3._1, L._3._3, D._1._1),
+    CornerColors(R._1._3, B._1._1, U._1._3), CornerColors(R._3._3, D._3._3, B._3._1),
+    CornerColors(B._1._3, L._1._1, U._1._1), CornerColors(B._3._3, D._3._1, L._3._1)
+  ))
 
-  private def standardizeCenterColors: Cube = {
-    val m = Map(
-      U.row2._2 -> 'W',
-      L.row2._2 -> 'G', F.row2._2 -> 'R', R.row2._2 -> 'B', B.row2._2 -> 'O',
-      D.row2._2 -> 'Y'
-    )
-    colorMap(m)
+  def canonicalize: Cube = standardizeCornerColors
+
+  private def standardizeCornerColors: Cube = colorMap(colorMapByCorners(cornerColors))
+}
+
+object Cube {
+  /** Standard corners coloring of a Rubik's cube.
+    *
+    * @see https://upload.wikimedia.org/wikipedia/commons/9/9d/Rubik%27s_cube_colors.svg
+    */
+  private val standardCornerColors = CornersColoring(Seq(
+    CornerColors('G','R','W'), CornerColors('B','W','R'), CornerColors('B','R','Y'), CornerColors('G','Y','R'),
+    CornerColors('B','O','W'), CornerColors('O','B','Y'), CornerColors('O','G','W'), CornerColors('O','Y','G')
+  ))
+
+  /** Map from a valid corners coloring to the color map that standardizes it. */
+  private val colorMapByCorners: Map[CornersColoring, Map[Char,Char]] = {
+    val colors = Seq('Y','R','G','O','B','W')
+    val allColorMaps = colors.permutations.map(_.zip(colors).toMap)
+
+    allColorMaps.map(m => {
+      val reverseMap = m.map{ case (k,v) => (v,k) }
+      val remappedCorners = CornersColoring(standardCornerColors.corners.map(_.colorMap(reverseMap)))
+      remappedCorners -> m
+    }).toMap
+  }
+}
+
+/** The colors of a corner, in counterclockwise order, permuted so that the least character comes first. */
+case class CornerColors private (_1: Char, _2: Char, _3: Char) {
+  def colorMap(m: Map[Char, Char]): CornerColors = CornerColors(m(_1), m(_2), m(_3))
+}
+
+object CornerColors {
+  implicit val ordering: Ordering[CornerColors] = Ordering[(Char,Char,Char)].on(x => (x._1, x._2, x._3))
+
+  def apply(_1: Char, _2: Char, _3: Char): CornerColors = {
+    assert(_1 != _2 && _1 != _3 && _2 != _3)
+
+    if (_1 < _2) {
+      if (_1 < _3) new CornerColors(_1, _2, _3)
+      else         new CornerColors(_3, _1, _2)
+    } else {  // _2 < _1
+      if (_2 < _3) new CornerColors(_2, _3, _1)
+      else         new CornerColors(_3, _1, _2)
+    }
+  }
+}
+
+case class CornersColoring private(
+  _1: CornerColors, _2: CornerColors, _3: CornerColors, _4: CornerColors,
+  _5: CornerColors, _6: CornerColors, _7: CornerColors, _8: CornerColors
+) {
+  def corners: Seq[CornerColors] = Seq(_1, _2, _3, _4, _5, _6, _7, _8)
+}
+
+//noinspection ZeroIndexToHead
+object CornersColoring {
+  def apply(corners: Seq[CornerColors]): CornersColoring = {
+    assert(corners.size == 8)
+
+    val cs = corners.sorted
+    new CornersColoring(cs(0), cs(1), cs(2), cs(3), cs(4), cs(5), cs(6), cs(7))
   }
 }
